50df8e35d1db9979dc0728aaa704b6d6c6cc5934
Diana C <diana@gmetrix.com>
2024-08-31 21:48:45 -0600

    adding new folder layout!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'WorkSansRegular': ['Work Sans', 'sans-serif'],
+        'WorkSansMedium': ['Work Sans Medium', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
