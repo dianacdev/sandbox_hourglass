3d13f6447f4318cf45b4e74135af7a2dc88113f4
Diana C <diana@gmetrix.com>
2024-09-01 11:57:13 -0600

    Added Titles, Descriptions, and empty images for onboarding screen.

---
+++ constants/index.ts
@@ -0,0 +1,20 @@
+export const onboarding = [
+    {
+        id: 1,
+        title: 'Welcome to Hourglass',
+        description: 'A simple and convinient way to track your time!',
+        image: null,
+    },
+    {
+        id: 2,
+        title: 'Log your time',
+        description: 'Using the categories you can log your time with ease!',
+        image: null,
+    },
+    {
+        id: 3,
+        title: 'Analyze your time',
+        description: 'An easy way to see how you spend your time!',
+        image: null,
+    },
+]
