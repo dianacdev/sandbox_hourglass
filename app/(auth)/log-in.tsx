098a41c6f9fed5ebc8ca857992ed346b00f120b6
Diana C <diana@gmetrix.com>
2024-09-01 14:03:52 -0600

    Updating Login & Signup Pages, No Clerk Logic Yet!

--- app/(auth)/log-in.tsx
+++ app/(auth)/log-in.tsx
@@ -1,13 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { ScrollView, View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images, icons } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const Login = () => {
-  return (
-    <SafeAreaView>
-      <Text>Login</Text>
-    </SafeAreaView>
-  )
-}
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const onLogInPress = async () => {
+    console.log(form);
+  };
+
+  return (
+    <ScrollView className="flex-1 bg-neutral-900">
+      <View className="flex-1 bg-neutral-900">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.DianaCLogo} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-bold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Example: JaneDoe@gmail.com"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value: any) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            icon={icons.lock}
+            placeholder="Enter your password"
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value: any) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Log in"
+            onPress={onLogInPress}
+            className="mt-6"
+          />
+          <OAuth />
+          <Link href="/sign-up" className="mt-10 text-center text-neutral-400">
+            <Text className="text-center">Need an account? </Text>
+            <Text className="text-center text-violet-400 underline">
+              Sign Up
+            </Text>
+          </Link>
+        </View>
+        {/* Verification Modal */}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Login;
---
+++ components/InputField.tsx
@@ -0,0 +1,42 @@
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from "react-native";
+
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}: InputFieldProps) => (
+  <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding": "height"}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="mr-2 w-full mt-1">
+        <Text className={`text-lg text-white font-bold mb-3 ${labelStyle}`}>
+          {label}
+        </Text>
+        <View
+          className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-neutral-500 ${containerStyle}`}
+        >
+            {icon && <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`}/>}
+            <TextInput className={`rounded-full p-4 font-bold text-[15px] flex-1 ${inputStyle} text-left`} secureTextEntry={secureTextEntry} {...props}>
+
+            </TextInput>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
+);
+
+export default InputField;
