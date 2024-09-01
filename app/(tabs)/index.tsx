96ae6013841179789284b8e2a62716b1d8b76641
Diana C <diana@gmetrix.com>
2024-08-31 19:54:02 -0600

    linter added, prettier added!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
